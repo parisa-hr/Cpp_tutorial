243405a1d1387d32634186077ab01040846e5513
Parisa <parisahamed76@gmail.com>
2022-12-09 14:30:52 +0330

    infint loop (menu)

--- codes/loops/main.cpp
+++ codes/loops/main.cpp
@@ -2,8 +2,45 @@
 
 using namespace std;
 
-int main()
+int  main()
 {
-    cout << "Hello World!" << endl;
+// for
+// while
+// do ... while
+
+    int  selector, a = 10, b = 20;
+
+    while (1)
+    {
+        cout << "1.Hello ra chap kon" << endl;
+        cout << "2. a + b " << endl;
+        cout << "3. Exit " << endl;
+        cout << "Az menu zir 1 gozineh ra Entekhab konid = ";
+
+        cin >> selector;
+
+        if (selector == 1)
+        {
+            cout << "Hello" << endl;
+            cout << "----------------------------------\n";
+        }
+        else if (selector == 2)
+        {
+            cout << "a+b" << a + b << endl;
+            cout << "----------------------------------\n";
+        }
+        else if (selector == 3)
+        {
+// return 0;
+// break;
+            exit(0);
+        }
+        else
+        {
+            cout << "Beyne 1 ta 3 bayad vared konid ." << endl;
+            cout << "----------------------------------" << endl;
+        }
+    }
+
     return 0;
 }
